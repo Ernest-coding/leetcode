d3c4330c28ebcf78d9a00f6ff014788719061c01
ernest_l <766188487@qq.com>
2021-09-23 17:21:54 +0800

    Initial commit

---
+++ src/com/leetcode/primary/array_1.java
@@ -0,0 +1,18 @@
+package com.leetcode.primary;
+
+public class array_1 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        int num = removeDuplicates(nums);
+    }
+
+    private static int removeDuplicates(int[] nums){
+        for (int i:
+             nums) {
+            System.out.println(i);
+        }
+        return 0;
+    }
+}
+
